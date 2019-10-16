4c488ecec3a0a1c7a32dfb7739318d0bb19c8199
Manbir Singh Rakhra <mrakhra@skedulo.com>
2019-10-16 14:03:02 +1000

    Working

---
+++ XamarinPluginMedia/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace XamarinPluginMedia
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+
+            takePhoto.Clicked += TakePhoto_ClickedAsync;
+            browsePhotos.Clicked += BrowsePhotos_Clicked;
+        }
+
+        private async void BrowsePhotos_Clicked(object sender, EventArgs e)
+        {
+                await CrossMedia.Current.Initialize();
+                if (!CrossMedia.Current.IsPickPhotoSupported)
+                {
+                    await DisplayAlert("Photos Not Supported", ":( Permission not granted to photos.", "OK");
+                    return;
+                }
+                var files = await CrossMedia.Current.PickPhotosAsync(new PickMediaOptions
+                {
+                    PhotoSize = PhotoSize.Medium
+                });
+
+
+                if (files == null)
+                    return;
+
+        }
+
+        private async void TakePhoto_ClickedAsync(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+        }
+    }
+}
